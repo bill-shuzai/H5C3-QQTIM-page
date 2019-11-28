b96b557291e7fcfee109e83101f63bf8c91ec33c
zachui <475684606@qq.com>
2019-11-28 12:28:39 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    $.stellar({
+       horizontalScrolling: false,
+       responsive: true
+    });
+
+    /*
+    * stellar使用方法
+    * 1.引入js文件
+    * 2.结构 data-stellar-background-ratio="0.3" 样式 bg 需要background-attachment:fixed;
+    * 3.初始换插件
+    * */
+});
